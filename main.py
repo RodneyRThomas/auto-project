aa5d033f5dc34c7764cb64c4d83a0e28e60fcc67
RodneyRThomas <82400756+RodneyRThomas@users.noreply.github.com>
2021-04-19 10:46:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+ger = "german"
+es = "spanish"
 
+print("Language")
+first = input()
+if first == ger:
+  print("Please type a project")
+  second = input()
+  if second = animal:
+    print("Tierprojekt")
+    print("IF YOU HAVE A DOG: Mein Hund ist süß, Mein Hund liebt es zu spielen")
