f0fa8bad2e8ae27280685ecfa5cb12166691a9ed
RodneyRThomas <82400756+RodneyRThomas@users.noreply.github.com>
2021-04-20 10:34:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 ger = "german"
 es = "spanish"
 
-print("Language")
+print "Language" 
 first = input()
 if first == ger:
-  print("Please type a project")
+  print "Please type a project"
   second = input()
   if second = covid:
-    print("Virus")
-    print("Tragen Sie Ihre Maske über Nase und Mund, um die Ausbreitung des Virus zu verhindern.") or print("Das Virus verbreitet sich schnell! Stoppen Sie die Ausbreitung, indem Sie Ihre Maske richtig tragen.")
+    print "Virus"
+    print "Tragen Sie Ihre Maske über Nase und Mund, um die Ausbreitung des Virus zu verhindern." or print "Das Virus verbreitet sich schnell! Stoppen Sie die Ausbreitung, indem Sie Ihre Maske richtig tragen."
